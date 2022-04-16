665956e4d9443cb9371d55740aabbcc32b85ad65
Ruslan <ruslanr721@gmail.ru>
2022-04-16 11:58:26 +0300

    Strings

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        String firstName  = ("Ivanov");
+        String middleName = ("Ivan");
+        String lastName = ("Ivanovich");
+        String fullName = (firstName + " " + middleName + " " + lastName);
+        System.out.println("ФИО сотрудника " + fullName);
+    }
+}
